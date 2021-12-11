a48f3ef880d8f211192b781135c37c0d01633fa4
Ellis Lunnon <ellislunnon@gmail.com>
2021-12-11 03:31:31 +0000

    Cleaned up directory

--- heartrate_app.py
+++
@@ -1,7 +0,0 @@
-from heartrate import trace, files
-trace(files = files.path_contains('Chess'), host='0.0.0.0', port='7777', browser=True)
-
-from Chess import Board
-
-b = Board()
-b.calculate()
--- trace_app.py
+++
@@ -1,4 +0,0 @@
-from Chess import Board
-
-b = Board()
-b.calculate()
